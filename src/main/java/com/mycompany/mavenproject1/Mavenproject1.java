c339977dc77718c50eedf9aef035a01c1c550f77
NahuelPajon <nahuel.pajon@correo.ucu.edu.uy>
2024-11-05 21:16:32 -0300

    Algunos ajustes y agregación de Metodo PRIM

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mavenproject1;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -11,6 +9,32 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        IVertice v1 = new TVertice("A");
+        IVertice v2 = new TVertice("B");
+        IVertice v3 = new TVertice("C");
+
+        IArista a1 = new TArista(v1.getEtiqueta(), v2.getEtiqueta(), 1.0);
+        IArista a2 = new TArista(v2.getEtiqueta(), v3.getEtiqueta(), 2.0);
+        IArista a3 = new TArista(v1.getEtiqueta(), v3.getEtiqueta(), 3.0);
+
+
+        ArrayList vertices = new ArrayList();
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        TAristas aristas = new TAristas();
+        aristas.add(a1);
+        aristas.add(a2);
+        aristas.add(a3);
+
+        TGrafoNoDirigido grafo = new TGrafoNoDirigido(vertices, aristas);
+
+        TGrafoNoDirigido mst = grafo.Prim();
+
+        System.out.println("Arbol abarcador minimo:");
+        for (IArista arista : mst.getLasAristas()) {
+            System.out.println(arista.getEtiquetaOrigen() + " - " + arista.getEtiquetaDestino() + " : " + arista.getCosto());
+        }
     }
 }
